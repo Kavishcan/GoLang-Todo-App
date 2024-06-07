ac3433f84745ff2afdd942fb4e2126622e8b2a8c
Kavishcan <kavishcan2002@gmail.com>
2024-06-08 00:37:38 +0530

    chore: Update BASE_URL to dynamically switch between development and production environments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
-import { Button, Stack, Container } from "@chakra-ui/react";
+import { Stack, Container } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 function App() {
   return (
     <Stack h="100vh">
